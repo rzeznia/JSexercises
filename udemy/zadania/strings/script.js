50854b7357165e59b43d7b79c6c82bfa3c5b6abf
rzeznia <marcin.gda_tier1@icloud.com>
2022-11-07 23:39:53 +0100

    strings

---
+++ udemy/zadania/strings/script.js
@@ -0,0 +1,33 @@
+/*
+    Metody do wykorzystania:
+    charAt()
+    includes()
+    replace()* podchwytliwe 🙂 
+    slice()
+    split()
+    toLowerCase()
+    toUpperCase()
+*/
+ 
+const text1 = 'powiększ mnie!'
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+const text4 = 'sprawdź, czy zawieram słowo "czy"'
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+const text7 = 'podziel, ten, string, od, przecinków'
+
+console.log(text1.toUpperCase())
+
+console.log(text2.toLowerCase())
+
+console.log(text3.slice(6))
+
+console.log(text4.includes('czy'))
+
+console.log(text5.split(" ")[0].replace(/[^L]/g, ""))
+
+console.log(text6.replaceAll('pies', 'kot'))
+
+console.log(text7.split(','))
+
